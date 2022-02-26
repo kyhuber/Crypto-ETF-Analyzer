afafe0feee442d090e473f9a521bdd410aac4145
kyhuber <kyhuber+ft@gmail.com>
2022-02-26 14:15:46 -0700

    Update app and mc functions

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from MCForecastTools import MCSimulation
 import matplotlib.pyplot as plt
 import yfinance as yf
+from mc import mcstreamlit
 
 # Set Webpage Name
 st.set_page_config(page_title="Crypto ETF Analysis", layout="centered")
@@ -18,7 +19,7 @@ crypto_choice = st.sidebar.selectbox(
     "Which cryptocurrency would you like to compare?",
     ("BTC-USD","ETH-USD","LTC-USD","XRP-USD"))
 cryptoData = yf.Ticker(crypto_choice)
-cryptoDf = cryptoData.history(period="id", start="2020-10-18",end="2021-2-18",index_col="Date", parse_dates=["Date"])
+cryptoDf = cryptoData.history(period="id", start="2020-10-18",index_col="Date", parse_dates=["Date"])
 
 #Crypto Cumulative Returns
 cryptoDf["Daily Return"] = cryptoDf.Close.pct_change()
@@ -28,11 +29,23 @@ crypto_cumulative_returns = (1 + cryptoDf["Daily Return"]).cumprod()
 ticker_choice = st.sidebar.text_input(
     "Which stock would you like to compare?")
 tickerData = yf.Ticker(ticker_choice)
-tickerDf = tickerData.history(period="id", start="2020-10-18", end="2021-2-18",index_col="Date", parse_dates=["Date"])
+tickerDf = tickerData.history(period="id", start="2020-10-18",index_col="Date", parse_dates=["Date"])
 
 #Ticker Cumulative Returns
 tickerDf["Daily Return"] = tickerDf.Close.pct_change()
 ticker_cumulative_returns = (1 + tickerDf["Daily Return"]).cumprod()
+
+#Gather other inputs
+initial_investment = st.sidebar.number_input(
+    "What is your initial investment?")
+weight1 = st.sidebar.number_input(
+    "What percent to your crypto investment?")
+weight2 = st.sidebar.number_input(
+    "What percent to your stock investment?")
+years = st.sidebar.number_input(
+    "How many years do you want to simulate")
+
+st.write(mcstreamlit(initial_investment,ticker_choice,crypto_choice,weight1,weight2,years))
 
 #Plot Both Cumulative Returns
 st.write(f"{crypto_choice} vs {ticker_choice} Cumulative Returns")
---
+++ mc.py
@@ -0,0 +1,36 @@
+import os
+from sqlite3 import Date
+import pandas as pd
+import requests
+import streamlit as st
+from dotenv import load_dotenv
+from datetime import datetime
+from MCForecastTools import MCSimulation
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+def mcstreamlit(initial_investment,ticker_choice,crypto_choice,weight1,weight2,years):
+    tickerData = yf.Ticker(ticker_choice)
+    tickerDf = tickerData.history(period="id", start="2020-10-18",index_col="Date", parse_dates=["Date"])
+    tickerDf.drop(["Dividends","Stock Splits"],axis=1,inplace=True)
+    tickerDf.columns = ["open","high","low","close","volume"]
+    cryptoData = yf.Ticker(crypto_choice)
+    cryptoDf = cryptoData.history(period="id", start="2020-10-18",index_col="Date", parse_dates=["Date"])
+    cryptoDf.drop(["Dividends","Stock Splits"],axis=1,inplace=True)
+    cryptoDf.columns = ["open","high","low","close","volume"]
+    to_merge_dict = {f"{ticker_choice}": tickerDf , f"{crypto_choice}": cryptoDf}
+
+    df = pd.concat(to_merge_dict.values(), axis=1, keys=to_merge_dict.keys())
+    
+    MC_df = MCSimulation(
+        portfolio_data = df.dropna(),
+        weights = [weight1,weight2],
+        num_simulation = 500,
+        num_trading_days = 252*years)
+    
+    x = MC_df.calc_cumulative_return()
+
+    output_summary=MC_df.summarize_cumulative_return()
+    percent_change = (output_summary["mean"])
+    final_investment=initial_investment*percent_change 
+    return f"if you invest in {ticker_choice} and {crypto_choice} with {weight1}%,{weight2}% with 100 dollars you would have ${final_investment:.2f}"
